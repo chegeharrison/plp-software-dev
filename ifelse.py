a2149420d046f716ffd81207662bac28a6e17ea3
harrry <chegeharrison10@gmail.com>
2025-03-25 21:21:19 +0300

    Week 2 Assignment

---
+++ ifelse.py
@@ -0,0 +1,25 @@
+temperature =27
+if temperature >=30:
+    print ("Its a hot day")
+elif 20 <= temperature < 30:
+    print("It's a warm day")
+else:
+    print("its a cold day")
+    
+#for loop
+basket_of_fruits = ['apple', 'banana', 'cherry', 'orange', 'kiwi','mango']
+for fruit in basket_of_fruits:
+    print(fruit)
+    
+#while loop
+count=4
+while count < 5:
+    print(count)
+    count += 1
+def add():
+    print("Hello World")
+add()
+def add(a,b):
+    sum = a+b
+    print(sum)
+add(45,55)
